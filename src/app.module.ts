565d7ba65e2d3c217dde692fbc078ebfbfe9de5c
Abdelilah_OU <gxgamer710@gmail.com>
2023-02-08 18:44:32 +0000

    add prisma service to app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './controllers/user.controller';
+import { PrismaService } from './services/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
