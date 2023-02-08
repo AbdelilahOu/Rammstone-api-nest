eb591a00be910b34acf0a43afaa8815329b87a35
Abdelilah_OU <gxgamer710@gmail.com>
2023-02-08 15:08:32 +0000

    add necessary controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientController } from './client/client.controller';
+import { SellerController } from './seller/seller.controller';
+import { ProductController } from './product/product.controller';
+import { CommandController } from './command/command.controller';
+import { InvoiceController } from './invoice/invoice.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ClientController, SellerController, ProductController, CommandController, InvoiceController],
   providers: [AppService],
 })
 export class AppModule {}
