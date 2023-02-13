660433ce52463a86bbcd0304d1d5973dde78fa80
Abdelilah_OU <gxgamer710@gmail.com>
2023-02-13 11:25:04 +0000

    containerize the app

--- app/src/services/auth.service.ts
+++ app/src/services/auth.service.ts
@@ -65,7 +65,7 @@ export class AuthService {
     const secret = this.config.get('JWT_SECRET');
 
     const access_token = await this.jwt.signAsync(payload, {
-      expiresIn: '15m',
+      expiresIn: '7d',
       secret,
     });
 
